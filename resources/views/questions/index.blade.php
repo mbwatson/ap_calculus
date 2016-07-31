c026844eb88a592e0c449a7bbede803c2f9293e8
mbwatson <mattwat@gmail.com>
2016-07-31 15:19:44 -0400

    question filtering menu

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -24,13 +24,19 @@
         
             <a class="btn btn-primary btn-block" href="{{ route('questions.create') }}">New Question</a>
             <br />
-            <h3 class="text-center">Filters</h3>
-            <a class="btn btn-info btn-block hidden-xs hidden-sm" href="{{ route('questions.index') }}">All</a>
-            <a class="btn btn-info btn-block hidden-xs hidden-sm" href="{{ route('questions.mine') }}">Mine</a>
-            <a class="btn btn-info btn-block hidden-xs hidden-sm disabled" href="{{ route('questions.popular') }}">Popular</a>
-            <a class="btn btn-info btn-block hidden-xs hidden-sm" href="{{ route('questions.calculator.active') }}">Calculator Active</a>
-            <a class="btn btn-info btn-block hidden-xs hidden-sm" href="{{ route('questions.calculator.inactive') }}">Calculator Inactive</a>
-            <a class="btn btn-info btn-block hidden-xs hidden-sm" href="{{ route('questions.calculator.neutral') }}">Calculator Neutral</a>
+            <div class="dropdown">
+                <button class="btn btn-info btn-block dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Filters
+                </button>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item btn btn-link btn-xs btn-block" href="{{ route('questions.index') }}">All Questions</a></li>
+                    <li><a class="dropdown-item btn btn-link btn-xs btn-block" href="{{ route('questions.mine') }}">My Questions</a></li>
+                    <li><a class="dropdown-item btn btn-link btn-xs btn-block disabled" href="{{ route('questions.popular') }}">Popular Questions</a></li>
+                    <li><a class="dropdown-item btn btn-link btn-xs btn-block" href="{{ route('questions.calculator.active') }}">Calculator Active</a></li>
+                    <li><a class="dropdown-item btn btn-link btn-xs btn-block" href="{{ route('questions.calculator.inactive') }}">Calculator Inactive</a></li>
+                    <li><a class="dropdown-item btn btn-link btn-xs btn-block" href="{{ route('questions.calculator.neutral') }}">Calculator Neutral</a></li>
+                </ul>
+            </div>
             <br />
         </div>
 
