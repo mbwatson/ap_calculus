98ae722c6194ebada17ea47f75880f872f3955b1
mbwatson <mattwat@gmail.com>
2016-07-30 20:34:27 -0400

    tiny fix in home page heading in jumbotron

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <!-- Welcome -->
 
     <div class="row">
-        <div class="col-xs-12 col-sm-10 col-sm-offset-1">
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 heading">
             <h1>AP Calculus Question Forum</h1>
             <h2>
                 This site is a collaboration space for AP Calculus AB and AP Calculus BC instructors.
