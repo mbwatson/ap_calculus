7f06378c8ed0d5b13cfe43468250cc0fd74006cb
mbwatson <mattwat@gmail.com>
2016-07-31 14:34:35 -0400

    todo list update

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -16,9 +16,7 @@
                         <li>regular conversation forum with categories?</li>
                         <li>tag/mention other users (with @-symbol?) in question comments or in regular conversation forum</li>
                         <li>tag standards [[LO 1.2A(b)]] or similar syntax in comments, and in possible regular forum</li>
-                        <li>implement way to distinguish between free response and multiple choice questions. (this probably will be a necessity.)</li>
-                        <li>if so, MC question creation form should provide form inputs for responses/distractors (and possibly marking the correct one)</li>
-                        <li>implement way to distinguish between calculator active/inactive/neutral questions</li>
+                        <li>multiple choice question creation form with inputs for responses/distractors (and possibly marking the correct one)</li>
                         <li>make profile public, optional</li>
                         <li>question voting system +/- 1 buttons</li>
                         <li>simple way to "suggest" standards without having to write a comment? or is a comment better?</li>
