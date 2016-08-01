c68244b2b9d9a2809026ecc92a612e0704942c15
mbwatson <mattwat@gmail.com>
2016-08-01 14:04:51 -0400

    update todo list

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -9,26 +9,62 @@
     <h1>To Do List / Fixes / Ideas</h1>
     
     <div class="row">
-        <div class="col-xs-12">
+        <div class="col-xs-12 col-sm-4">
             <div class="panel panel-default">
                 <div class="panel-body">
-                    <ul>
-                        <li>implement the addition of scoring guidelines with question</li>
-                        <li>regular conversation forum with categories?</li>
-                        <li>tag/mention other users (with @-symbol?) in question comments or in regular conversation forum</li>
-                        <li>tag standards [[LO 1.2A(b)]] or similar syntax in comments, and in possible regular forum</li>
-                        <li>multiple choice question creation form with inputs for responses/distractors (and possibly marking the correct one)</li>
-                        <li>make profile public, optional</li>
-                        <li>question voting system +/- 1 buttons</li>
-                        <li>simple way to "suggest" standards without having to write a comment? or is a comment better?</li>
-                        <li>bootstrap breaking point for navbar--probably need to finalize navbar first</li>
-                        <li>search: toggle between union and intersection of keywords/standards</li>
-                        <li>slugs, esp. for standards</li>
-                        <li>figure out what more should be in the sidebar, or if it should exist at all</li>
-                        <li>assistance for inexperienced $\LaTeX$ users</li>
-                        <li>ElasticSearch</li>
-                        <li>improve flash messages, possibly with Flashy</li>
-                    </ul>
+                    <div class="panel-heading">
+                        Big Ideas
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li>implement the addition of scoring guidelines with question</li>
+                            <li>regular conversation forum with categories?</li>
+                            <li>question voting system +/- 1 buttons</li>
+                            <li>simple way to "suggest" standards without having to write a comment? or is a comment better?</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+                    
+        <div class="col-xs-12 col-sm-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="panel-heading">
+                        Backend Improvements
+                    </div>
+                    <div class="panel-body">
+                        <ul>
+                            <li>make profile public, optional</li>
+                            <li>multiple choice question creation form with inputs for responses/distractors (and possibly marking the correct one)</li>
+                            <li>tag standards [[LO 1.2A(b)]] or similar syntax in comments, and in possible regular forum</li>
+                            <li>tag/mention other users (with @-symbol?) in question comments or in regular conversation forum</li>
+                            <li>slugs, esp. for standards</li>
+                            <li>ElasticSearch</li>
+                            <li>improve flash messages, possibly with Flashy</li>
+                            <li>ability to flag questions as spam</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+                    
+        <div class="col-xs-12 col-sm-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <div class="panel-heading">
+                        UX Improvements
+                    </div>
+                
+                    <div class="panel-body">
+                        <ul>
+                            <li>bootstrap breaking point for navbar--probably need to finalize navbar first</li>
+                            <li>search: toggle between union and intersection of keywords/standards</li>
+                            <li>figure out what more should be in the sidebar, or if it should exist at all</li>
+                            <li>assistance for inexperienced $\LaTeX$ users</li>
+                            <li>improve dashboard UX</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
