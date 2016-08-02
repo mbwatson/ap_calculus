b38f144a96ff07eb123bbb6aa9ac700dace738e6
mbwatson <mattwat@gmail.com>
2016-08-02 11:15:02 -0400

    add discussion & response models

---
+++ app/Discussion.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discussion extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var Array
+     */
+    protected $fillable = [
+        'title', 'body', 'user_id'
+    ];
+    
+    /**
+     * Retrieve the user that owns the discussion
+     *
+     * @return App\User
+     */
+    public function user()
+    {
+    	return $this->belongsTo('App\User');
+    }
+
+    /**
+     * Retrieve the responses that belong to the discussion
+     *
+     * @return Illuminate\Database\Eloquent\Collection
+     */
+    public function responses()
+    {
+    	return $this->hasMany('App\Response');
+    }
+
+}
