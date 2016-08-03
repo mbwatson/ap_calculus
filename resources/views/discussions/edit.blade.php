e1b7418036a8fafa837a8835c9ac00348462279e
mbwatson <mattwat@gmail.com>
2016-08-03 12:44:57 -0400

    add tinymce to forms

--- resources/views/discussions/edit.blade.php
+++ resources/views/discussions/edit.blade.php
@@ -4,8 +4,57 @@
 
 @section('breadcrumbs', Breadcrumbs::render('discussions.edit', $discussion))
 
-@section('content')
+@section('header')
+<!-- TinyMCE -->
+<script src='//cdn.tinymce.com/4/tinymce.min.js'></script>
+<script>
+  var editor_config = {
+    path_absolute : "/",
+    selector: '#question-textarea',
+    plugins: 'advlist autolink image lists charmap preview textpattern',
+    toolbar: 'undo redo | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist | link image | preview',
+    menubar: false,
+    relative_urls: false,
+    textpattern_patterns: [
+      {start: '*', end: '*', format: 'italic'},
+      {start: '**', end: '**', format: 'bold'},
+      {start: '#', format: 'h1'},
+      {start: '##', format: 'h2'},
+      {start: '###', format: 'h3'},
+      {start: '####', format: 'h4'},
+      {start: '#####', format: 'h5'},
+      {start: '######', format: 'h6'},
+      {start: '1. ', cmd: 'InsertOrderedList'},,
+      {start: '* ', cmd: 'InsertUnorderedList'},
+      {start: '- ', cmd: 'InsertUnorderedList'}
+    ],
+    file_browser_callback : function(field_name, url, type, win) {
+      var x = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;
+      var y = window.innerHeight|| document.documentElement.clientHeight|| document.getElementsByTagName('body')[0].clientHeight;
 
+      var cmsURL = editor_config.path_absolute + 'laravel-filemanager?field_name=' + field_name;
+      if (type == 'image') {
+        cmsURL = cmsURL + "&type=Images";
+      } else {
+        cmsURL = cmsURL + "&type=Files";
+      }
+
+      tinyMCE.activeEditor.windowManager.open({
+        file : cmsURL,
+        title : 'Filemanager',
+        width : x * 0.8,
+        height : y * 0.8,
+        resizable : "yes",
+        close_previous : "no"
+      });
+    }
+  };
+
+  tinymce.init(editor_config);
+</script>
+@endsection
+
+@section('content')
 <div class="container">
 
     <!-- Discussion Edit Form -->
@@ -50,5 +99,4 @@
     </div>
 
 </div>
-
 @endsection
