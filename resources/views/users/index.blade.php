ddcd9f67f789787505bc60e12b750b452fb16720
mbwatson <mattwat@gmail.com>
2016-08-05 00:32:44 -0400

    user list page: css and breadcrumbs

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,15 +1,16 @@
 @extends('layouts.master')
 
+@section('title', 'Users')
+
+@section('breadcrumbs', Breadcrumbs::render('users.index'))
+
 @section('content')
 
 <!-- User List -->
 
 <div class="container">
 
-    <h1>
-        Users
-        <small>All users registered on this site.</small>
-    </h1>
+    <h1>Registered Users</h1>
 
     <!-- Users List -->
     
@@ -18,14 +19,21 @@
             <div class="col-xs-12 col-sm-4 col-md-2">
                 <div class="panel panel-default">
                     <div class="panel-body text-center">
-                        @include('partials.user-card', ['user' => $user])
+                        <div class="{{ $user->isOnline() ? 'active' : '' }}-user">
+                            <a href="{{ route('users.show', $user) }}">
+                                <img class="avatar" src="{{ url('/') }}/avatars/{{ $user->avatar }}"><br />
+                                <span class="username">{{ $user->name }}</span>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         @endforeach
     </div>
 
-    <footer><span class="fa fa-check-circle"></span> indicates a user is currently logged in.</footer>
+    <div class="row">
+        <span class="fa fa-check-circle"></span> indicates a user is currently logged in.
+    </div>
 </div>
 
 @endsection
