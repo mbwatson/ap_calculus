b3fddee4b4d1fc80a6253aa4406bcdf6559458ec
mbwatson <mattwat@gmail.com>
2016-08-07 13:11:19 -0400

    home page adjusting

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,23 @@
 
     <div class="row">
         <div class="col-xs-12 col-sm-10 col-sm-offset-1 heading">
-            <h1>AP Calculus Question Forum</h1>
-            <h2>
+            <h1 style="font-weight: 700;">AP Calculus Question Forum</h1>
+        </div>
+        <div class="col-xs-12 col-sm-10 col-sm-offset-1 details">
+            <p>
                 This site is a collaboration space for AP Calculus AB and AP Calculus BC instructors.
-            </h2>
-            <h2>
+            </p>
+            <p>
                 Create and collaborate with others in your field to design quality questions that promote the understanding of calculus and&mdash;of course&mdash;make these questions available to others.
-            </h2>
-            <h2>
+            </p>
+            <p>
                 The tools and expertise provided by this community of experts make it simple to shape questions that
                 align with the Curriculum Framework defining the AP Calculus curriculum, as laid out by the CollegeBoard.
-            </h2>
-            <div class="text-center" style="padding: 25px;">
-                <a class="btn btn-white btn-lg" href="{{ route('questions.index') }}" style="margin:0 25px">Browse the Questions</a>
-                <a class="btn btn-warning btn-lg" href="{{ route('standards.index') }}" style="margin:0 25px">Curriculum Framework</a>
-            </div>
+            </p>
+        </div>
+        <div class="text-center" style="padding: 25px;">
+            <a class="btn btn-white btn-lg" href="{{ route('questions.index') }}" style="margin:25px;">Browse the Questions</a>
+            <a class="btn btn-warning btn-lg" href="{{ route('standards.index') }}" style="margin:25px;">Curriculum Framework</a>
         </div>
     </div>
 </div>
