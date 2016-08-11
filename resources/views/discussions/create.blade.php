8a5cec299451966e3878ca628e836f5de2a15c06
mbwatson <mattwat@gmail.com>
2016-08-10 23:27:53 -0400

    extract discussion form

--- resources/views/discussions/create.blade.php
+++ resources/views/discussions/create.blade.php
@@ -62,36 +62,7 @@
     <div class="row" id="discussion">
         <div class="col-xs-12">
             {!! Form::open(['route' => 'discussions.store']) !!}
-
-            {!! Form::text('title', null, ['class' => 'form-control form-title', 'placeholder' => 'Enter Title Here']) !!}
-
-            <div class="form-group" style="background-color: #000; padding: 1em; color: #fff;">
-                <div class="row">
-                    <label for="channel_id" class="col-xs-3" style="font-weight: bold;">DISCUSSIONS / </label>
-                    <div class="col-xs-8">
-                        {!! Form::select('channel_id', $channels_list, null, ['placeholder' => 'Channel', 'class' => 'form-control']) !!}
-                    </div>
-                </div>
-            </div>
-
-            <div class="form-group">
-                <div class="row" id="post">
-                    <div class="col-xs-2 text-center">
-                        <div class="{{ Auth::user()->isOnline() ? 'active-' : '' }}user text-center">
-                            <img class="avatar" src="{{ url('/') }}/avatars/{{ Auth::user()->avatar }}"><br />
-                            <span class="username">{{ Auth::user()->name }}</span>
-                        </div>
-                    </div>
-                    <div class="col-xs-10">
-                        {!! Form::textarea('body', null, ['id' => 'discussion-textarea', 'class' => 'form-control body', 'placeholder' => 'Please add a short title above, choose a channel in which to post your discsussion, and type being your discussion in this textbox.']) !!}
-                    </div>
-                </div>
-            </div>
-
-            <br />
-
-            {!! Form::submit('Post Discussion', ['class' => 'btn btn-primary btn-block']) !!}
-            
+              @include('partials.forms.discussion')            
             {!! Form::close() !!}
         </div>
     </div>
