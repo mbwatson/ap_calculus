60d951f91ab483081305003b9aecdbcc400ba11e
mbwatson <mattwat@gmail.com>
2016-08-11 20:36:47 -0400

    improve favorite js

--- public/src/js/favorites.js
+++ public/src/js/favorites.js
@@ -1,5 +1,10 @@
 $('a.favorite').click(function(evt) {
 	evt.preventDefault();
+	if ($(this).hasClass('text-muted')) {
+		$(this).attr('title', 'Remove from Favorites').tooltip('fixTitle').tooltip('show');
+	} else {
+		$(this).attr('title', 'Add to Favorites').tooltip('fixTitle').tooltip('show');
+	}
 	$(this).toggleClass("text-muted text-primary");
 	$(this).children('i').toggleClass("mdi-heart mdi-heart-outline");
 	$.ajax({
