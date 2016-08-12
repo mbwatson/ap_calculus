2599bafc1281d7dfc7eb3b31ee43b4c5fb6476e2
mbwatson <mattwat@gmail.com>
2016-08-12 13:15:28 -0400

    fix likes on responses

--- resources/views/partials/response.blade.php
+++ resources/views/partials/response.blade.php
@@ -25,10 +25,10 @@
                 <!-- Thumbs Up -->
                 @if ($response->liked(Auth::user()))
                     <a href="{{ route('responses.unlike', $response) }}" class="text-primary" title="Unlike" data-toggle="tooltip" data-placement="top">
-                        <i class="glyphicon glyphicon-thumbs-up"></i></a>
+                        <i class="mdi mdi-thumb-up mdi-18px"></i></a>
                 @else
                     <a href="{{ route('responses.like', $response) }}" class="text-muted" title="Like" data-toggle="tooltip" data-placement="top">
-                        <i class="glyphicon glyphicon-thumbs-up"></i></a>
+                        <i class="mdi mdi-thumb-up-outline mdi-18px"></i></a>
                 @endif
                 <sub>{{ $response->likeCount }}</sub>
             </div>
