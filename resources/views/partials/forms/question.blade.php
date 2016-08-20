d139238322f30daa9e3e503f4402ec3e936d0b3e
mbwatson <mattwat@gmail.com>
2016-08-20 12:03:49 -0400

    fix change to snake case for question type

--- resources/views/partials/forms/question.blade.php
+++ resources/views/partials/forms/question.blade.php
@@ -1,10 +1,10 @@
 <div class="row">
   <div class="btn-group col-md-5" data-toggle="buttons" style="display: flex;">
       <label class="btn btn-primary {{ (isset($question->type) && $question->type == 'free_response') ? 'active' : ''}}" style="flex: 1;">
-          <input type="radio" name="type" autocomplete="off" value="1"  {{ (isset($question->type) && $question->type == 'Free Response') ? 'checked' : ''}}> <span class="fa fa-pencil-square-o"></span> Free Response
+          <input type="radio" name="type" autocomplete="off" value="1"  {{ (isset($question->type) && $question->type == 'free-Response') ? 'checked' : ''}}> <span class="fa fa-pencil-square-o"></span> Free Response
       </label>
       <label class="btn btn-primary {{ (isset($question->type) && $question->type == 'multiple_choice') ? 'active' : ''}}" style="flex: 1;">
-          <input type="radio" name="type" autocomplete="off" value="2" {{ (isset($question->type) && $question->type == 'Multiple Choice') ? 'checked' : ''}}> <span class="fa fa-list"></span> Multiple Choice
+          <input type="radio" name="type" autocomplete="off" value="2" {{ (isset($question->type) && $question->type == 'multiple_choice') ? 'checked' : ''}}> <span class="fa fa-list"></span> Multiple Choice
       </label>
   </div>
   <div class="btn-group col-md-5 col-md-offset-1" data-toggle="buttons" style="display: flex;">
