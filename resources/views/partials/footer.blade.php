2d719b87013c7a0befe93088153fea026811c168
mbwatson <mattwat@gmail.com>
2016-08-21 12:42:04 -0400

    format footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,18 @@
 <div class="container">
 	<div class="row">
-		<div class="col-xs-12 col-sm-4 col-sm-offset-2">
+		<div class="col-xs-12 col-sm-4">
 			&#169; 2016 &mdash; AP Calculus Community Forum <br /><br />
 			Site created and maintained by <a href="#">Matthew Watson</a>
 		</div>
-		<div class="col-xs-12 col-sm-5 col-sm-offset-1">
+		<div class="col-xs-12 col-sm-3 col-sm-offset-1">
 			<ul class="links">
 				<li><a href="{{ route('standards.index') }}">Curriculum Framework</a></li>
+				<li><a href="{{ url('standards.mpacs') }}">Mathematical Practices</a></li>
+				<li><a href="{{ url('standards.big-ideas') }}">Big Ideas</a></li>
+			</ul>
+		</div>
+		<div class="col-xs-12 col-sm-3 col-sm-offset-1">
+			<ul class="links">
 				<li><a href="{{ route('questions.index') }}">Questions</a></li>
 				<li><a href="{{ route('discussions.index') }}">Discussions</a></li>
 			</ul>
